41c016058edcbcfddc6f3297d09800c7ae5f67d2
PREMC4 <118727787+PREMC4@users.noreply.github.com>
2022-11-21 16:37:54 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,11 @@
+def base(n):
+    l=[]
+    while(n>0):
+        rem=n%3
+        l.append(rem)
+        n=n//3
+    for i in range(len(l)-1,-1,-1):
+        print(l[i],end="")
+
 n=int(input())
-l=[]
-while(n>0):
-    rem=n%3
-		l.append(rem)
-		n=n//3
-for i in range(len(l)-1,-1,-1):
-    print(l[i],end="")
+base(n)
