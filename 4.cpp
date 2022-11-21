38dbb27fdcff67e96d2cfc4bea17acc1d94d78a3
PREMC4 <118727787+PREMC4@users.noreply.github.com>
2022-11-21 16:44:11 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a[256][256],r,c;
+  cin >> r >> c;
+  for(int i=0;i<r;i++)
+  {
+    for(intj=0;j<c;j++)
+    {
+      cin >> a[i][j];
+    }
